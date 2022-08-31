59c77162b8a960bab06883987b44aa6890005035
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-08-31 15:25:13 -0300

    asd

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.set('view engine', 'pug')
 
 // EJS desconmentar linea 32
-// app.set("view engine", ".ejs");
+app.set("view engine", ".ejs");
 
 app.set("views", "./views");
 
