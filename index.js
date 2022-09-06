b750ef125263fd91deca077ea561c4863bc938ba
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-09-06 00:05:59 -0300

    finish challenge class 12

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const express = require("express");
+// websockets
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
 const routes = require("./routes/index");
 const { storeProducts } = require("./controllers/products");
 const path = require("path");
 const { engine } = require("express-handlebars");
-const pug = require("pug");
-const ejs = require("ejs");
+// const pug = require("pug");
+// const ejs = require("ejs");
+const messages = [];
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // HANDLEBARS desconmentar linea 17 a 26
-// app.engine(
-//   "hbs",
-//   engine({
-//     extname: ".hbs",
-//     defaultLayout: "",
-//     layoutsDir: __dirname + "/views/layouts",
-//     partialsDir: __dirname + "/views/partials",
-//   })
-// );
-// app.set("view engine", ".hbs");
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+app.set("view engine", ".hbs");
 
-// PUG desconmentar linea 29
-// app.set('view engine', 'pug')
-
-// EJS desconmentar linea 32
-app.set("view engine", ".ejs");
+// WEBSOCKETS
+const connectedServer = httpServer.listen(8080, () =>
+  console.log("server http con web socket listen on port 8080")
+);
+connectedServer.on("error", (error) => {
+  console.log("error", error);
+});
 
 app.set("views", "./views");
 
@@ -41,10 +51,27 @@ app.get("/listProducts", async (_req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.render("form", { scriptPath: "./js/main.js" });
 });
 
 app.listen(PORT, () => {
   console.log(`APP listen on port: ${PORT}`);
 });
+
+
+io.on("connection", (socket) => {
+  socket.emit("getData", storeProducts);
+  socket.on("newProduct", (prod) => {
+    storeProducts.push(prod);
+    io.sockets.emit("updateProducts", storeProducts);
+  });
+  socket.emit("getMessages", messages);
+  socket.on('newMessage', (msg) => {
+    messages.push(msg);
+    io.sockets.emit("updateMessages", messages);
+  });
+});
+
+
+
