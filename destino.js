a216b1a8c751a74439018cd8570287fea89d75f3
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-09-08 13:31:14 -0300

    class-13

---
+++ destino.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var obj = {
+  name: 'nikin'
+};
+var name = obj === null || obj === void 0 ? void 0 : obj.name;
---
+++ origin.js
@@ -0,0 +1,3 @@
+const obj = { name: 'nikin'};
+
+const name = obj?.name
