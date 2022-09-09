20cf784b9dbb4591c15dd7891eab1017ba0959b6
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-09-09 02:33:45 -0300

    details routing not founded

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", routes);
-app.get("*", function (_req, res) {
-    res.status(404).json({ message: "page not found" }).send("Page not found");
+app.use("*", function (req, res) {
+    return res.status(404).json({ error: -2, descripcion: `ruta: ${req.url} - metodo: ${req.method} no implementada` });
 });
 app.listen(PORT, () => {
     console.log(`APP listen on port: ${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-app.get("*", function (_req, res) {
-  res.status(404).json({ message: "page not found" }).send("Page not found");
+app.use("*", function (req, res) {
+  return res.status(404).json({ error : -2, descripcion: `ruta: ${req.url} - metodo: ${req.method} no implementada`});
 });
 
 app.listen(PORT, () => {
