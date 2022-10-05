f3b797b0f580b87d124f07e1a8a4b9ec07ec4ad7
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-10-05 15:11:47 -0300

    create branch

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import productsRoutes from './products.js';
-
-const router = express.Router();
-
-router.use('/products', productsRoutes);
-
-export default router;
--- store/products.js
+++
@@ -1,3 +0,0 @@
-const storeProducts = [];
-
-module.exports = storeProducts;
